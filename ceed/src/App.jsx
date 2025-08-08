18acb531181dbea07e6825518ed24cd07ea8c8bd
stevenchenstevenchen <stevenchen2632@gmail.com>
2025-08-08 17:47:03 -0400

    New Update

--- ceed/src/App.jsx
+++ ceed/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import './dist/styles.css';
 import Navbar from './components/Navbar';
-import Members from './components/Members'
+// import Members from './components/Members'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import Recruitment from './components/Recruitment'
